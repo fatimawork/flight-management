fddf696af2cfe5bc794bf51eaaf3793c58ccfb0c
Fatima El mourtaki <fatimaelmourtaki@gmail.com>
2024-05-01 22:38:18 +0100

    changes

--- Brief-1_Gestion-du-Vol/Frontend/flight-frontend/src/pages/Extras.jsx
+++ Brief-1_Gestion-du-Vol/Frontend/flight-frontend/src/pages/Extras.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from '../components/Header'
 import ExtrasForm from '../components/ExtrasForm'
+import Footer from '../components/Footer'
 
 export default function Extras() {
   return (
     <div>
         <Header header="Extras" />
         <ExtrasForm />
+        <Footer />
+
     </div>
   )
 }
--- Brief-1_Gestion-du-Vol/Frontend/flight-frontend/src/pages/Payment.jsx
+++ Brief-1_Gestion-du-Vol/Frontend/flight-frontend/src/pages/Payment.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import PaymentForm from '../components/PaymentForm'
+import Footer from '../components/Footer'
 
 export default function Payment() {
   return (
     <div>
         <Header header="Payment" />
         <PaymentForm />
+        <Footer />
 
     </div>
   )
