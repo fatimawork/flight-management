4f30b1ecf4f85dd9d25b779c9df43b0f71c8f3da
LATIFADEV <latifaaamouguay@gmail.com>
2024-05-02 10:24:08 +0100

    add

--- Brief-1_Gestion-du-Vol/Backend/server/config/db_config.js
+++ Brief-1_Gestion-du-Vol/Backend/server/config/db_config.js
@@ -1,9 +1,11 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 // Initialize Sequelize with your database connection details
-const sequelize = new Sequelize('vol_management', 'root', '', {
+const sequelize = new Sequelize('vol_management', 'root', 'latifa123', {
   host: 'localhost',
+  port: 3306,
   dialect: 'mysql',
+  
 });
 
 exports.sequelize = sequelize;
