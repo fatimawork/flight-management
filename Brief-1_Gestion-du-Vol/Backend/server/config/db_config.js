9a6fc27cbafb50e4c5b399f020acd918f62446ed
sikimara <driss.shi.shi@gmail.com>
2024-05-03 13:50:32 +0100

    last update

--- Brief-1_Gestion-du-Vol/Backend/server/config/db_config.js
+++ Brief-1_Gestion-du-Vol/Backend/server/config/db_config.js
@@ -1,9 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 // Initialize Sequelize with your database connection details
-const sequelize = new Sequelize('vol_management', 'root', 'root', {
+const sequelize = new Sequelize('vol_management', 'root', '', {
   host: 'localhost',
-  port: '3309',
+  port: '3306',
   dialect: 'mysql',
 });
 
