b3a2a029e20d576317780f38ee83703206b856b0
Fatima El mourtaki <fatimaelmourtaki@gmail.com>
2024-05-06 12:23:23 +0100

    port

--- Brief-1_Gestion-du-Vol/Backend/server/config/db_config.js
+++ Brief-1_Gestion-du-Vol/Backend/server/config/db_config.js
@@ -1,9 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 // Initialize Sequelize with your database connection details
-const sequelize = new Sequelize('vol_management', 'root', '', {
+const sequelize = new Sequelize('vol_management', 'root', 'root', {
   host: 'localhost',
-  port: '3306',
+  port: '3309',
   dialect: 'mysql',
 });
 
